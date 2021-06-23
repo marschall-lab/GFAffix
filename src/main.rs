11dd1c710bbd413b7f776fe1044200758b56f741
Daniel Doerr <ddoerr@cebitec.uni-bielefeld.de>
2021-06-23 10:26:57 +0200

    record graph transformation in event tracker

--- src/main.rs
+++ src/main.rs
@@ -93,35 +93,48 @@ impl DeletedSubGraph {
 
 #[derive(Clone, Debug)]
 pub struct CollapseEventTracker {
-    pub visited: FxHashSet<Handle>,
+    pub transform: FxHashMap<Handle, Vec<Handle>>,
     pub overlapping_events: usize,
     pub bubbles: usize,
     pub events: usize,
 }
 
 impl CollapseEventTracker {
-    fn report(&mut self, graph: &HashGraph, affix: &AffixSubgraph, collapsed_prefix_node: Handle) {
+    fn report(&mut self, collapsed_prefix_node: Handle, shared_prefix_nodes: &Vec<Handle>,
+        splitted_node_pairs : &Vec<(Handle, Option<Handle>)>) {
         self.events += 1;
-        let prefix_len = graph.sequence_vec(collapsed_prefix_node).len();
-        let is_bubble = !affix
-            .shared_prefix_nodes
-            .iter()
-            .any(|v| graph.sequence_vec(*v).len() != prefix_len);
-        for v in affix.shared_prefix_nodes.iter() {
-            if self.visited.contains(v) || (is_bubble && self.visited.contains(&v.flip())) {
+        let is_bubble = !splitted_node_pairs.iter().all(|(_, x)| *x == None);
+        if is_bubble {
+            self.bubbles += 1;
+        }
+        for i in 0..shared_prefix_nodes.len() {
+            let v = shared_prefix_nodes[i];
+            if self.transform.contains_key(&v) || (is_bubble && self.transform.contains_key(&v.flip())) {
                 self.overlapping_events += 1
             }
-        }
-        self.visited.insert(collapsed_prefix_node);
-        if is_bubble {
-            self.visited.insert(collapsed_prefix_node.flip());
-            self.bubbles += 1;
+
+            // record transformation of node if any took place (which is not the case if node v
+            // equals the dedicated shared prefix node 
+            if v != collapsed_prefix_node {
+                let mut replacement : Vec<Handle> = Vec::new();
+                replacement.push(collapsed_prefix_node);
+                if let Some(u) = splitted_node_pairs[i].1 {
+                    replacement.push(u)
+                }
+                self.transform.insert(v, replacement.clone());
+                if is_bubble {
+                    // if shared prefix is a bubble than also record the reverse complementary
+                    // transformation
+                    self.transform.insert(v.flip(), replacement.iter().map(|u|
+                            u.flip()).collect());
+                }
+            }
         }
     }
 
     fn new() -> Self {
         CollapseEventTracker {
-            visited: FxHashSet::default(),
+            transform: FxHashMap::default(),
             overlapping_events: 0,
             bubbles: 0,
             events: 0,
@@ -194,23 +207,21 @@ fn collapse(
     graph: &mut HashGraph,
     shared_prefix: &AffixSubgraph,
     del_subg: &mut DeletedSubGraph,
+    event_tracker : &mut CollapseEventTracker
 ) -> Handle {
     let prefix_len = shared_prefix.sequence.len();
 
     // update graph in two passes:
     //  1. split nodes into shared prefix and distinct suffix and appoint dedicated shared
     //  prefix node
-    let mut shared_prefix_node_maybe: Option<Handle> = None;
+    let mut shared_prefix_node_pos : usize = 0;
     let mut splitted_node_pairs: Vec<(Handle, Option<Handle>)> = Vec::new();
-    for v in shared_prefix.shared_prefix_nodes.iter() {
+    for (i, v) in shared_prefix.shared_prefix_nodes.iter().enumerate() {
         if graph.sequence_vec(*v).len() < prefix_len {
             // x corresponds to the shared prefix,
             let (x, u) = graph.split_handle(*v, prefix_len);
             splitted_node_pairs.push((x, Some(u)));
             // update dedicated shared prefix node if none has been assigned yet
-            if shared_prefix_node_maybe == None {
-                shared_prefix_node_maybe = Some(x);
-            }
             log::debug!(
                 "splitting node {}{} into prefix {}{} and suffix {}{}",
                 if v.is_reverse() { '<' } else { '>' },
@@ -223,7 +234,7 @@ fn collapse(
         } else {
             // always use a node as dedicated shared prefix node if that node coincides with the
             // prefix
-            shared_prefix_node_maybe = Some(*v);
+            shared_prefix_node_pos = i; 
             splitted_node_pairs.push((*v, None));
             log::debug!(
                 "node {}{} matches prefix {}",
@@ -237,7 +248,7 @@ fn collapse(
     //  2. update deleted edge set, reassign outgoing edges of "empty" nodes to dedicated shared
     //     prefix node
     // there will be always a shared prefix node, so this condition is always true
-    let shared_prefix_node = shared_prefix_node_maybe.unwrap();
+    let shared_prefix_node = shared_prefix.shared_prefix_nodes[shared_prefix_node_pos];
     log::debug!(
         "node {}{} is dedicated shared prefix node",
         if shared_prefix_node.is_reverse() {
@@ -248,14 +259,14 @@ fn collapse(
         usize::from(shared_prefix_node.id())
     );
 
-    for (u, maybe_v) in splitted_node_pairs {
-        if u != shared_prefix_node {
+    for (u, maybe_v) in splitted_node_pairs.iter() {
+        if *u != shared_prefix_node {
             // rewrire outgoing edges
             match maybe_v {
                 Some(v) => {
                     // make all suffixes spring from shared suffix node
-                    if !graph.has_edge(shared_prefix_node, v) {
-                        graph.create_edge(Edge(shared_prefix_node, v));
+                    if !graph.has_edge(shared_prefix_node, *v) {
+                        graph.create_edge(Edge(shared_prefix_node, *v));
                         log::debug!(
                             "create edge {}{}-{}{}",
                             if shared_prefix_node.is_reverse() {
@@ -274,7 +285,7 @@ fn collapse(
                     // node), then all outgoing edges of this node must be transferred to dedicated
                     // node
                     let outgoing_edges: Vec<Handle> = graph
-                        .neighbors(u, Direction::Right)
+                        .neighbors(*u, Direction::Right)
                         .filter(|v| !del_subg.is_deleted(&u, v))
                         .collect();
                     for w in outgoing_edges {
@@ -301,9 +312,12 @@ fn collapse(
                 if u.is_reverse() { '<' } else { '>' },
                 usize::from(u.id())
             );
-            del_subg.add_node(u, &graph);
+            del_subg.add_node(*u, &graph);
         }
     }
+
+    event_tracker.report(shared_prefix_node, &shared_prefix.shared_prefix_nodes,
+        &splitted_node_pairs);
 
     shared_prefix_node
 }
@@ -369,8 +383,8 @@ fn find_and_report_variant_preserving_shared_affixes<W: Write>(
                         queue.extend(affix.parents.iter().filter(|u| !del_subg.nodes.contains(u)));
                     } else {
                         print(affix, out)?;
-                        let shared_prefix_node = collapse(graph, affix, &mut del_subg);
-                        event_tracker.report(graph, &affix, shared_prefix_node);
+                        let shared_prefix_node = collapse(graph, affix, &mut del_subg, &mut
+                            event_tracker);
                         queue.push_back(shared_prefix_node);
                         queue.push_back(shared_prefix_node.flip());
                     }
