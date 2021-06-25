5ebcac6893225afdeb7266af55621e28e8498246
Daniel Doerr <ddoerr@cebitec.uni-bielefeld.de>
2021-06-25 15:47:00 +0200

    improved detection of overlapping events

--- src/main.rs
+++ src/main.rs
@@ -533,7 +533,7 @@ fn find_and_report_variant_preserving_shared_affixes<W: Write>(
                         print(affix, out)?;
                         if affix.parents.iter().chain(affix.shared_prefix_nodes.iter()).any(|&u| {
                             event_tracker
-                                .transform
+                                .modified_nodes
                                 .contains_key(&(u.unpack_number() as usize, graph.node_len(u)))
                         }) {
                             event_tracker.overlapping_events += 1
